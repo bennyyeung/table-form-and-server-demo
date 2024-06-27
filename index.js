48d85c7cc5175ed55a43b2589a1c4a007a929ecd
Benny Yeung <benny.tf@gmail.com>
2024-06-27 16:08:36 +0100

    Database and form submission setup

--- index.js
+++ index.js
@@ -17,9 +17,28 @@ const app = express();
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'))
 
+app.use(express.urlencoded({entended: true}))
+app.use(express.json());
+
 app.get('/', (req, res) => {
+    res.render('home')
+})
+
+app.get('/register', (req, res) => {
     res.render('register')
 })
+
+app.post('/register', async (req, res) => {
+    try {
+        console.log(req.body);
+        const user = new User(req.body);
+        await user.save();
+        res.redirect('/');
+    } catch (error) {
+        console.error(error);
+        res.status(500).send('Oh no, Error!!');
+    }
+});
 
 app.listen(3000, ()=> {
     console.log("On Port 3000!")
--- models/user.js
+++ models/user.js
@@ -1,37 +1,39 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
+// const UserSchema = new Schema({
+//     username: {
+//         type: String,        
+//     },
+//     //password not hashed yet
+//     password: {
+//         type: String,
+//         unique: true
+//     },
+//     address: {
+//         type: String,
+//     },
+//     sports: {
+//         type: String,
+//     },
+//     gender: {
+//         type: String,
+//     },
+//     ageGroup: {
+//         type: String,
+//     },
+//     photo: {
+//         type: String,
+//     }
+// });
+
 const UserSchema = new Schema({
-    username: {
-        type: String,
-        required: true,        
-    },
-    //password not hashed yet
-    password: {
-        type: String,
-        required: true,
-        unique: true
-    },
-    address: {
-        type: String,
-        required: true,
-    },
-    sports: {
-        type: String,
-        required: true,
-    },
-    gender: {
-        type: String,
-        required: true,
-    },
-    ageGroup: {
-        type: String,
-        required: true,
-    },
-    photo: {
-        type: String,
-        required: true,
-    }
+    username: String,
+    password: String,
+    address: String,
+    sports: String,
+    gender: String,
+    ageGroup: String
 });
 
 module.exports = mongoose.model('User', UserSchema);
